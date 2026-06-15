803a4043223b7b64fbd1446d40ec0b5dc8a950b4
lubegar2 <lubegar2@aulumni.uv.es>
2026-06-15 12:44:13 +0200

    Tappa 04 Completed - 3D space architecture

---
+++ build/CMakeFiles/Tappa04.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tappa04.dir/src/tappa04.cpp.obj"
+  "CMakeFiles/Tappa04.dir/src/tappa04.cpp.obj.d"
+  "Tappa04.exe"
+  "Tappa04.exe.manifest"
+  "Tappa04.pdb"
+  "libTappa04.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tappa04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
