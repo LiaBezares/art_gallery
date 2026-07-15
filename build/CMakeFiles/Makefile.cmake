aa680e4a57995083f17392beb90b540c7270b381
lubegar2 <lubegar2@aulumni.uv.es>
2026-07-15 20:30:52 +0200

    Tappa 08 - Completed - Walking and Wall Collision

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -73,5 +73,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/Tappa07.dir/DependInfo.cmake"
+  "CMakeFiles/Tappa08.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/Tappa08.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tappa08.dir/src/tappa08.cpp.obj"
+  "CMakeFiles/Tappa08.dir/src/tappa08.cpp.obj.d"
+  "Tappa08.exe"
+  "Tappa08.exe.manifest"
+  "Tappa08.pdb"
+  "libTappa08.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tappa08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
